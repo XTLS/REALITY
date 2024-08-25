7f458161d5f92a4bec7e8c13b9b3b906cd1c43df
yuhan6665 <1588741+yuhan6665@users.noreply.github.com>
2024-08-25 11:04:56 -0400

    crypto/tls: add ech client support
    This CL adds a (very opinionated) client-side ECH implementation.
    
    In particular, if a user configures a ECHConfigList, by setting the
    Config.EncryptedClientHelloConfigList, but we determine that none of
    the configs are appropriate, we will not fallback to plaintext SNI, and
    will instead return an error. It is then up to the user to decide if
    they wish to fallback to plaintext themselves (by removing the config
    list).
    
    Additionally if Config.EncryptedClientHelloConfigList is provided, we
    will not offer TLS support lower than 1.3, since negotiating any other
    version, while offering ECH, is a hard error anyway. Similarly, if a
    user wishes to fallback to plaintext SNI by using 1.2, they may do so
    by removing the config list.
    
    With regard to PSK GREASE, we match the boringssl  behavior, which does
    not include PSK identities/binders in the outer hello when doing ECH.
    
    If the server rejects ECH, we will return a ECHRejectionError error,
    which, if provided by the server, will contain a ECHConfigList in the
    RetryConfigList field containing configs that should be used if the user
    wishes to retry. It is up to the user to replace their existing
    Config.EncryptedClientHelloConfigList with the retry config list.
    
    Fixes #63369
    
    Cq-Include-Trybots: luci.golang.try:gotip-linux-amd64-longtest
    Change-Id: I9bc373c044064221a647a388ac61624efd6bbdbf
    Reviewed-on: https://go-review.googlesource.com/c/go/+/578575
    Reviewed-by: Ian Lance Taylor <iant@google.com>
    Reviewed-by: Filippo Valsorda <filippo@golang.org>
    Reviewed-by: Than McIntosh <thanm@google.com>
    Reviewed-by: Dmitri Shuralyov <dmitshur@golang.org>
    Auto-Submit: Roland Shoemaker <roland@golang.org>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

--- conn.go
+++ conn.go
@@ -75,6 +75,7 @@ type Conn struct {
 	// resumptionSecret is the resumption_master_secret for handling
 	// or sending NewSessionTicket messages.
 	resumptionSecret []byte
+	echAccepted      bool
 
 	// ticketKeys is the set of active session ticket keys for this
 	// connection. The first one is used to encrypt new tickets and
@@ -1713,6 +1714,7 @@ func (c *Conn) connectionStateLocked() ConnectionState {
 	} else {
 		state.ekm = c.ekm
 	}
+	state.ECHAccepted = c.echAccepted
 	return state
 }
 
