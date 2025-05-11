5052e9a93c7487c86796509c3e1818bb78613f82
yuhan6665 <1588741+yuhan6665@users.noreply.github.com>
2025-05-10 23:30:06 -0400

    crypto/tls: use crypto/hkdf For consistency, prefer crypto/hkdf over crypto/internal/fips140/hkdf. Both should have the same behavior given the constrained use of HKDF in TLS.
    Change-Id: Ia982b9f7a6ea66537d748eb5ecae1ac1eade68a5
    Reviewed-on: https://go-review.googlesource.com/c/go/+/658217
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Carlos Amedee <carlos@golang.org>
    Reviewed-by: Roland Shoemaker <roland@golang.org>

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.24
 
 require (
 	github.com/pires/go-proxyproto v0.8.1
-	golang.org/x/crypto v0.37.0
-	golang.org/x/sys v0.32.0
+	golang.org/x/crypto v0.38.0
+	golang.org/x/sys v0.33.0
 )
--- handshake_server_tls13.go
+++ handshake_server_tls13.go
@@ -625,13 +625,13 @@ func (hs *serverHandshakeStateTLS13) doHelloRetryRequest(selectedGroup CurveID) 
 		if err := transcriptMsg(helloRetryRequest, confTranscript); err != nil {
 			return nil, err
 		}
-		b, _ := hkdf.Extract(hs.suite.hash.New, hs.clientHello.random, nil)
-		acceptConfirmation := tls13.ExpandLabel(hs.suite.hash.New,
-			b,
-			"hrr ech accept confirmation",
-			confTranscript.Sum(nil),
-			8,
-		)
+		h := hs.suite.hash.New
+		prf, err := hkdf.Extract(h, hs.clientHello.random, nil)
+		if err != nil {
+			c.sendAlert(alertInternalError)
+			return nil, err
+		}
+		acceptConfirmation := tls13.ExpandLabel(h, prf, "hrr ech accept confirmation", confTranscript.Sum(nil), 8)
 		helloRetryRequest.encryptedClientHello = acceptConfirmation
 	}
 
@@ -789,13 +789,13 @@ func (hs *serverHandshakeStateTLS13) sendServerParameters() error {
 			return err
 		}
 		// compute the acceptance message
-		b, _ := hkdf.Extract(hs.suite.hash.New, hs.clientHello.random, nil)
-		acceptConfirmation := tls13.ExpandLabel(hs.suite.hash.New,
-			b,
-			"ech accept confirmation",
-			echTranscript.Sum(nil),
-			8,
-		)
+		h := hs.suite.hash.New
+		prk, err := hkdf.Extract(h, hs.clientHello.random, nil)
+		if err != nil {
+			c.sendAlert(alertInternalError)
+			return err
+		}
+		acceptConfirmation := tls13.ExpandLabel(h, prk, "ech accept confirmation", echTranscript.Sum(nil), 8)
 		copy(hs.hello.random[32-8:], acceptConfirmation)
 	}
 
