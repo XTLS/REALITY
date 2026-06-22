e5b675196f2ec63c7d380290e78d7056957c07fc
Fan Yang <fanyang@microsoft.com>
2026-06-22 00:12:19 +0000

    Increase REALITY target TLS record buffer to 16 KiB

--- tls.go
+++ tls.go
@@ -137,7 +137,7 @@ func NewRatelimitedConn(conn net.Conn, limit *LimitFallback) net.Conn {
 }
 
 var (
-	size  = 8192
+	size  = 16384
 	empty = make([]byte, size)
 	types = [7]string{
 		"Server Hello",
